d4fbfe95e7df151337def644aa520f0afc01f071
stepsame <stepsame@gmail.com>
2015-10-16 16:33:33 +0800

    finish the course

--- database_setup.py
+++ database_setup.py
@@ -5,47 +5,59 @@ from sqlalchemy import create_engine
  
 Base = declarative_base()
 
-class Restaurant(Base):
-    __tablename__ = 'restaurant'
-   
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+	__tablename__ = 'user'
 
-    @property
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-       }
+	id = Column(Integer, primary_key = True)
+	name = Column(String(250), nullable = False)
+	email = Column(String(250), nullable = False)
+	picture = Column(String(250))
+
+class Restaurant(Base):
+	__tablename__ = 'restaurant'
+   
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
+
+	@property
+	def serialize(self):
+	   """Return object data in easily serializeable format"""
+	   return {
+		   'name'         : self.name,
+		   'id'           : self.id,
+	   }
  
 class MenuItem(Base):
-    __tablename__ = 'menu_item'
+	__tablename__ = 'menu_item'
 
 
-    name =Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
-    description = Column(String(250))
-    price = Column(String(8))
-    course = Column(String(250))
-    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant)
+	name =Column(String(80), nullable = False)
+	id = Column(Integer, primary_key = True)
+	description = Column(String(250))
+	price = Column(String(8))
+	course = Column(String(250))
+	restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
+	restaurant = relationship(Restaurant)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
 
 
-    @property
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'description'         : self.description,
-           'id'         : self.id,
-           'price'         : self.price,
-           'course'         : self.course,
-       }
+	@property
+	def serialize(self):
+	   """Return object data in easily serializeable format"""
+	   return {
+		   'name'         : self.name,
+		   'description'         : self.description,
+		   'id'         : self.id,
+		   'price'         : self.price,
+		   'course'         : self.course,
+	   }
 
 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
  
 
 Base.metadata.create_all(engine)
